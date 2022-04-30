4568cf594a0b5de1ccee2702ff709cb6889519f6
gjjblucky <pahadion007@gmail.com>
2022-04-30 17:28:55 +0530

    done

---
+++ POPUP/script.js
@@ -0,0 +1,13 @@
+const op=document.getElementById('open');
+const cl=document.getElementById('close');
+const d=document.getElementById('container');
+
+op.addEventListener('click',() => {
+container.classList.add('active');
+});
+
+cl.addEventListener('click',() => {
+    container.classList.remove('active');
+    });
+
+
