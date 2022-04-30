70f1ce36fe2094e236a5b1ff61292203626317f1
gjjblucky <pahadion007@gmail.com>
2022-04-30 15:43:41 +0530

    done

---
+++ TOAST NOTIFICATION/script.js
@@ -0,0 +1,26 @@
+const btn=document.getElementById('btn');
+const container=document.getElementById('container');
+btn.addEventListener('click',() =>{
+
+    createNotification();
+}
+)
+function createNotification() {
+    const notif=document.createElement('div');
+
+    notif.innerText="you are owsmmm";
+
+notif.classList.add('toast');
+     container.appendChild(notif);
+
+     setTimeout(() => {
+         notif.remove();
+     },3000);
+}
+    
+
+
+
+
+
+
