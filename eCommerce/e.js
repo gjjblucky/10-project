937d2f4115a1a0ee2d320c849c435bbfad7901eb
gjjblucky <pahadion007@gmail.com>
2022-05-06 16:43:00 +0530

    done

--- eCommerce/e.js
+++ eCommerce/e.js
@@ -68,4 +68,46 @@ parentContainer.addEventListener('click',(e)=>{
     }
 })
 
+window,addEventListener('DOMContentLoaded',() =>{
+  axios.get('http://localhost:4000/products',).then((data) =>{
+      console.log(data);
+     if(data.request.status === 200)
+     {
+         const products =data.data.products;
+         const parentSection =document.getElementById('Products');
+         products.forEach(product => {
+             const productHtml=`
+             <div>
+<h1>${product.title}</h1>
+<img src =${product.imageUrl} ></img>
+<button onClick="addToCart(${product.id})" >Add To Cart</button>
 
+             </div>`
+             parentSection.innerHTML+= productHtml;
+         });
+     }
+  })  
+})
+
+function addToCart(productId){
+    axios.post('http://localhost:4000/cart',{productId: productId})
+    .then(response =>{
+        if(response.status === 200){
+            notifyUsers(response.data.message);   
+        }
+    })
+    .catch(err =>{
+        notifyUsers(err.data.message);
+    })
+}
+
+function notifyUsers(message){
+    const container = document.getElementById('container');
+    const notification = document.createElement('div');
+    notification.classList.add('notification');
+    notification.innerHTML = `<h4>${message}</h4>`;
+    container.appendChild(notification);
+    setTimeout(()=>{
+        notification.remove();
+    },2500)
+}
